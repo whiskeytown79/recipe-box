b28f261a52415e143ae7ac14a0f8f9f5731e8458
Jake Cohen <cohen.jacob@gmail.com>
2021-02-13 21:44:22 -0800

    Some tweaks:

---
+++ RecipeBox/Models/Recipe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecipeBox.Models
+{
+    public class Recipe
+    {
+        public Recipe()
+        {
+        }
+
+        private List<string> steps;
+    }
+}
